e34fe7a73bccda5b9e930dd7ef55a89f7431300f
yungANDREYblud <115628068+yungANDREYblud@users.noreply.github.com>
2023-04-12 11:56:48 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -69,3 +69,17 @@ for (const item_tab of btn_tabs) {
         }
     })
 }
+
+
+let uls = document.querySelectorAll('.items-pricing ul')
+
+for (const ul of uls) {
+    ul.addEventListener('click', function(){
+        for (const iterator of uls) {
+            iterator.classList.remove("active")
+            iterator.querySelector('li:nth-child(3)') .classList.remove("active")
+        }
+        ul.classList.add('active')
+        ul.querySelector('li:nth-child(3)') .classList.add("active")
+    })
+}
